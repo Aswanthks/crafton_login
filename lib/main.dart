73428bc945e818ca8bdec91178d733c7ab8e95ac
aswan <aswanthks31@gmail.com>
2024-02-24 14:46:23 +0530

    iniitial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:crafton_login/screens/screen1.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MaterialApp(
+      home: screen1()));
+}
---
+++ lib/screens/screen1.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class screen1 extends StatelessWidget {
+  const screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      body:Container(
+        margin: EdgeInsets.all(20),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+        ),
+      )
+
+      
+
+    );
+  }
+}
